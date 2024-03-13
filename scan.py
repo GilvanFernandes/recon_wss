d60b99dd2e4d4143778221d004ce45e60a8ea8e9
Gilvan Fernandes <gilvan@heavycodes.com>
2024-03-12 21:53:18 -0300

    Atualização do execute_subfinder e nmap

--- scan.py
+++ scan.py
@@ -1,23 +1,55 @@
 import subprocess 
 import argparse 
-
+import pandas as pd
+import nmap
 
 # Kevin
 # Objetivo: varredura nas portas, varredura nas portas abertas 
-def execute_namp():
+def execute_namp(target):
+
+    scanner = nmap.PortScanner()
+
+    scanner.scan(target)
+
+    # Print the scan results
+    for host in scanner.all_hosts():
+        print("Host: ", host)
+        print("State: ", scanner[host].state())
+        for proto in scanner[host].all_protocols():
+            print("Protocol: ", proto)
+            ports = scanner[host][proto].keys()
+            for port in ports:
+                print("Port: ", port, "State: ", scanner[host][proto][port]['state'])
 
 # Martines
 # Objetivo: varredura de diretorios, 
 def execute_feroxbuster():
 
+
+
+    
+    print("execute_feroxbuster...")
+
 # Gilvan
 # Objetivo: enumera subdominio
-def execute_subfinder():
+def execute_subfinder(target):
+
+    command = f'subfinder -d {target} '
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode == 0:
+        print("Saída Subfinder:\n", stdout.decode())
+    else:
+        print("Erro Subfinder:\n", stderr.decode())
 
 # Will
 # Objetivo: 
 def execute_nuclei():
 
+
+
+    
+    print("execute_nuclei...")
 
 
 def main():
@@ -26,10 +58,12 @@ def main():
     
     args = parser.parse_args()
 
-    execute_nmap(args.target)
+    execute_namp(args.target)
     execute_feroxbuster(args.target)
+    execute_subfinder(args.target)
+    execute_nuclei(args.target)
 
 if __name__ == "__main__":
     main()
 
-python3 scan.py https://sap.com
+# Para executar o script no terminal: python3 scan.py https://sap.com
