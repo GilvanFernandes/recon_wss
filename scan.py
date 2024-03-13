342f6190b67535410092e7590c79e463368643f9
martines.pavani <msspavani@gmail.com>
2024-03-12 21:54:08 -0300

    adicionando funcao para executar  feroxbuster

--- scan.py
+++ scan.py
@@ -1,22 +1,58 @@
 import subprocess 
 import argparse 
-
+import subprocess
+from datetime import datetime
+import time
+import os
 
 # Kevin
 # Objetivo: varredura nas portas, varredura nas portas abertas 
-def execute_namp():
+# def execute_namp():
 
 # Martines
 # Objetivo: varredura de diretorios, 
-def execute_feroxbuster():
+def execute_feroxbuster(target):
+    
+    print('[+] Executando feroxbuster...')
+    start_time = time.time()  
+    clean_target = target.replace('https://', '').replace('http://', '')
+    date = time.strftime("%Y%m%d", time.localtime())
+    output_file = f"scan_result/{clean_target}_feroxbuster_{date}.txt"
+    
+   
+    assets_path = os.path.join(os.path.dirname(__file__), "assets")
+    wordlist_path = os.path.join(assets_path, "wordlist.txt")
+    
+    if os.path.exists(wordlist_path):
+        command = ["feroxbuster", "-u", target, "-w", wordlist_path, "-r"]
+    else:
+        command = ["feroxbuster", "-u", target, "-r"]
+    
+    with open(output_file, "w") as f:
+        subprocess.run(command, stdout=f, stderr=subprocess.PIPE)
+
+    end_time = time.time()  
+    execution_time = end_time - start_time  
+    print(f'[+] Feroxbuster finalizado! Tempo de execução: {execution_time:.2f} segundos')
+
+
 
 # Gilvan
 # Objetivo: enumera subdominio
-def execute_subfinder():
+# def execute_subfinder():
 
-# Will
-# Objetivo: 
-def execute_nuclei():
+# # Will
+# # Objetivo: 
+# def execute_nuclei():
+
+
+
+def check_feroxbuster_installed():
+    try:
+        subprocess.run(["which", "feroxbuster"], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 
 
@@ -26,10 +62,10 @@ def main():
     
     args = parser.parse_args()
 
-    execute_nmap(args.target)
+    #execute_nmap(args.target)
     execute_feroxbuster(args.target)
 
 if __name__ == "__main__":
     main()
 
-python3 scan.py https://sap.com
+##python3 scan.py https://sap.com
